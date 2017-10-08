a4b15ecd4fbafa90b94f47bf193387fb3edc30ed
sminev@ramapo.edu <sminev@ramapo.edu>
2017-10-08 18:30:17 -0400

    Created a new remove_tiles function that removes the first N elements from a vector of tiles. Now both players' hands are saved in local variables in the main file and the deck is updated when they draw their hands

--- longana.lisp
+++ longana.lisp
@@ -1,17 +1,35 @@
 (load "tile.lisp")
 
 ;(print_tiles ( shuffle (create_deck 0 0) 0))
-(let (
+( let* (
         (
             deck 
-            (shuffle (create_deck 0 0))
+            (initialize_deck 0 0)
+            
+        )
+        (
+            human_hand
+            (draw_hand deck 0 0)
+        ) 
+        (  
+            computer_hand
+            (draw_hand deck 0 1)
+        )
+        (
+            deck
+            (remove_tiles 16 deck)
         )
      )
-    (print_tiles (remove '(1 1) deck :test #'equal))
-    (terpri)
+    ;(print_tiles (remove '(1 1) deck :test #'equal))
+    ;(terpri)
     ;(print_tiles (draw_hand deck 0 0))
+    (print "Deck: ")
     (print_tiles deck)
     (terpri)
-    (print_tiles (draw_hand deck 0 1))
+    (print "Human Hand: ")
+    (print_tiles human_hand)
+    (terpri)
+    (print "Computer Hand:")
+    (print_tiles computer_hand)
 )
 ;(print (list 1 2 3))
--- tile.lisp
+++ tile.lisp
@@ -9,13 +9,13 @@
 (defun is_double (pips)
         (equal (first pips) (first(rest pips))))
 
-(defun create_deck (pip1 pip2)
+(defun initialize_deck (pip1 pip2)
     (cond ( (and (= pip1 7) (= pip2 7))
             ())
           ( (= pip2 6)
-            (append (create_deck (+ pip1 1) (+ pip1 1)) (list(list pip1 pip2))) )
+            (append (initialize_deck (+ pip1 1) (+ pip1 1)) (list(list pip1 pip2))) )
           ( t
-            (append (create_deck pip1 (+ pip2 1)) (list(list pip1 pip2))) )))
+            (shuffle (append (initialize_deck pip1 (+ pip2 1)) (list(list pip1 pip2))) ))))
 
 (defun shuffle(deck) 
     ;(print_deck deck)
@@ -41,26 +41,24 @@
           )
 )      
 
+(defun remove_tiles(numElements vector)
+    (cond ( (= numElements 0)
+           vector)
+          ( t 
+            (remove_tiles (- numElements 1) (rest vector))
+          )
+    )
+)
+
 (defun draw_hand(deck iteration playerIndex)
     (cond ( (= iteration 8)
             ())
           ( (= playerIndex 0)
-             (append (list(first deck)) (draw_hand (rest(rest deck)) (+ iteration 1) 0)))
+             (cons (first deck) (draw_hand (rest(rest deck)) (+ iteration 1) 0)))
           ( (= playerIndex 1)
-             (append (list (first (rest deck))) (draw_hand (rest(rest deck)) (+ iteration 1) 1)))
+             (cons (first (rest deck)) (draw_hand (rest(rest deck)) (+ iteration 1) 1)))
           ( t
           deck)))
-
-
-(defun append_at(index dest vector)
-    (cond ( (= index dest)
-            (append (rest vector) (list(first vector))))
-          ( t 
-            (append (list(first vector)) (append_at (+ index 1) dest (rest vector)) )))
-            
-)
-
-
 
 (defun print_tiles(vector)
     (cond ( (null vector)
