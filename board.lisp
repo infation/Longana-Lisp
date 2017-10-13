cf7914f76c3a56abcc5c875d352299e5bc81ce98
sminev@ramapo.edu <sminev@ramapo.edu>
2017-10-13 19:02:16 -0400

    made a function switch_pips which switches the pips of the chosen tile if the 1st pip is equal to the board 's first pip (1 hour)

--- board.lisp
+++ board.lisp
@@ -2,14 +2,26 @@
 
 (defun add_to_board(element vector side)
     (cond 
-          ( (or (equal side "L") (null vector) ) 
-            (print_board (cons element vector))
-            (cons element vector))
+          ( (or (equal side "L") (null vector)) 
+            (print_board (cons (switch_pips element vector) vector))
+            (cons (switch_pips element vector) vector))
           ( (equal side "R")
-             (print_board (cons vector (list element)))
-             (cons vector (list element)))
+             (print_board (cons vector (switch_pips element vector)))
+             (cons vector (switch_pips element vector)))
           ( t
             ))
+)
+
+(defun switch_pips(element board)
+    (cond 
+        ( (equal (first (rest element)) (first (first board)))
+          element
+        )
+        (t
+          (list (first (rest element)) (first element))
+        )
+    
+    )
 )
 
 (defun print_board(vector)
@@ -23,13 +35,13 @@
 (defun check_left_rules(element vector)
     (cond
         ( ( or (null vector) 
-          (equal (first (rest element)) (first (first vector)) )
-          (equal (first element) (first(first vector)) )) 
-          t)
+          (equal (first (rest element)) (first (first vector)) ) 
+          (equal (first element) (first(first vector)) ))
+            t)
         (t 
           nil
-        ))
-
+        )
+    )
 )
 
 (defun check_available_moves(board hand)
